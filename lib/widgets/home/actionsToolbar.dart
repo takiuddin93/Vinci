52033cb933c033478a50f4df18276ff9c67487db
Taki Uddin <takiuddin93@gmail.com>
2020-12-20 19:15:18 +0600

    new UI features

--- lib/widgets/home/actionsToolbar.dart
+++ lib/widgets/home/actionsToolbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ActionsToolbar extends StatelessWidget {
   @override
@@ -6,6 +7,7 @@ class ActionsToolbar extends StatelessWidget {
     return Container(
       width: 100.0,
       child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+        _getFollowAction(),
         _getSocialAction(
             iconData: Icons.favorite, title: '3.2m', isShare: false),
         _getSocialAction(
@@ -15,10 +17,68 @@ class ActionsToolbar extends StatelessWidget {
     );
   }
 
+  Widget _getFollowAction() {
+    return Container(
+      width: 60.0,
+      height: 60.0,
+      margin: EdgeInsets.only(top: 10.0),
+      child: Stack(
+        children: <Widget>[_getProfilePicture(), _getAddIcon()],
+      ),
+    );
+  }
+
+  Widget _getProfilePicture() {
+    return Positioned(
+      left: 5.0,
+      child: Container(
+        height: 50.0,
+        width: 50.0,
+        padding: EdgeInsets.all(1.0),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(25.0)),
+        child: CachedNetworkImage(
+          imageUrl:
+              'https://2.bp.blogspot.com/-iXsSQbijS2c/Tyw4Hz_BsWI/AAAAAAAADY4/koZlEyBVUN4/s1600/Ace+Ventura+3.gif',
+          imageBuilder: (context, imageProvider) => Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: imageProvider,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          placeholder: (context, url) => new CircularProgressIndicator(),
+          errorWidget: (context, url, error) => new Icon(Icons.error),
+        ),
+      ),
+    );
+  }
+
+  Widget _getAddIcon() {
+    return Positioned(
+      bottom: 0.0,
+      left: 5.0,
+      child: Container(
+        width: 20.0,
+        height: 20.0,
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 255, 43, 84),
+            borderRadius: BorderRadius.circular(16.0)),
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 20.0,
+        ),
+      ),
+    );
+  }
+
   Widget _getSocialAction({IconData iconData, String title, bool isShare}) {
     return Container(
       width: 60.0,
       height: 60.0,
+      margin: EdgeInsets.only(top: 10.0),
       child: Column(
         children: <Widget>[
           Icon(
